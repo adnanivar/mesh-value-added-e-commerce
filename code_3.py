db033571e93833f16f93af0fd485a10292b197bf
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2024-02-02 18:12:14 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,40 @@
+    }
+    return data;
+
+    const data = [];
+function main() {
+}
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+}
+    data.forEach(item => {
+    const data = generateRandomData();
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+
+function generateRandomData() {
+    });public class RandomDataGenerator {
+        int[] data = new int[10];
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
+}
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+            System.out.println("Random Number: " + item);
+        }        for (int i = 0; i < 10; i++) {
+        }
+}
+        }
+        for (int item : data) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    }
